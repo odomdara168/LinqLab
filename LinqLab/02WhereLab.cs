8896ebc8e04b62cbfb8ac6cd7298e873266cb5f9
dara.odom <dara.odom@ms101s-MacBook-Pro.local>
2025-05-06 09:55:32 +0700

    Initial commit from my local project

---
+++ LinqLab/02WhereLab.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLab
+{
+    public class WhereLab
+    {
+        private static IEnumerable<Sample> Source { get; set; }
+        public WhereLab()
+        {
+            Source = new SampleDate().GetData();
+        }
+        public List<Sample> WhereToGetIdBiggerThen8()
+        {
+            var result = Source.Where(i => i.Id > 8);
+            return result.ToList();
+        }
+
+        public List<Sample> WhereToGetPrizeBiggerThen200()
+        {
+            var result = Source.Where(i => i.Price > 200);
+                
+            return result.ToList();
+        }
+
+        public List<Sample> WhereToGetUserNameStartWithD()
+        {
+            var result = Source.Where(i => i.UserName.StartsWith("d"));
+            return result.ToList();
+        }
+
+        public List<Sample> WhereToGetUserNameContainE()
+        {
+            var result = Source.Where(i => i.UserName.Contains("e"));
+            return result.ToList();
+        }
+
+        public List<Sample> WhereToGetUserNameEndWithO()
+        {
+            var result = Source.Where(i => i.UserName.EndsWith("o"));
+            return result.ToList();
+        }
+
+        public List<Sample> WhereToGetUserNameEqualToDemoAndJoey()
+        {
+            var whereStr = new[] {"demo","joey" };
+            var result = Source.Where(i => whereStr.Contains(i.UserName));
+            return result.ToList();
+        }
+
+        public bool WhereToCheckIsAnyIdEqualTo99()
+        {
+            return Source.Any(i => i.Id == 99);
+        }
+    }
+}
---
+++ LinqLab/05OrderByLab.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqLab
+{
+    public class OrderByLab
+    {
+        private static IEnumerable<Sample> Source { get; set; }
+        public OrderByLab()
+        {
+            Source = new SampleDate().GetData();
+        }
+
+        public List<Sample> AscendingId()
+        {
+            var result = Source.OrderBy(x => x.Id).ToList();
+            return result;
+        }
+
+        public List<Sample> DescendingId()
+        {
+            var result = Source.OrderByDescending(x => x.Id).ToList();
+            return result;
+        }
+
+        public List<Sample> AscendingPriceAndThenAscendingId()
+        {
+            var result = Source.OrderBy(x => x.Price).ThenBy(x => x.Id).ToList();
+            return result;
+        }
+    }
+}
